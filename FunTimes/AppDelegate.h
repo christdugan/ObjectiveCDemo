6456ef81b18d4af997bfba09d339c92c60df33bf
Chris Dugan <christophertdugan@gmail.com>
2014-04-16 14:55:40 -0400

    Initial Commit

---
+++ FunTimes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FunTimes
+//
+//  Created by Christopher Dugan on 4/16/14.
+//  Copyright (c) 2014 Flyby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FunTimesTests/FunTimesTests.m
@@ -0,0 +1,34 @@
+//
+//  FunTimesTests.m
+//  FunTimesTests
+//
+//  Created by Christopher Dugan on 4/16/14.
+//  Copyright (c) 2014 Flyby. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FunTimesTests : XCTestCase
+
+@end
+
+@implementation FunTimesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
